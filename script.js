4cf2028537fb70503bb34bd2c4d4011ea1d1a619
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2024-12-15 10:11:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// let rounded = Math.round(4.7);
+// let roundedDown = Math.round(4.3);
+alert('sizda hozir random sonlar chiqishni boshlaydi😉')
+let realNumber = Math.floor(Math.random() * 1000);
+alert(`random orqali chiqqan son: ${realNumber}`)
+alert('biz random sonni 2 ga kopaytirmoqchimiz va bolmoqchimiz!')
+console.log("Tasodifiy son:", realNumber);
+let result = realNumber * 2;
+console.log("Ko'paytirilgan natija:", result);
+let natija = realNumber / 2;
+console.log("bo'lingan natija:", natija);
+
+alert(`random orqali chiqqan son: ${realNumber} edi\n\nuni 2ga ko'paytirsak: ${result} boladi\n\nuni 2ga bolsak ${natija} bo'ladi\n\nuni o'zidan bitta kam songa bo'lsak qoldiq har doim 1 chiqadi`)
+
+// let ceiling = Math.ceil(4.3);
+// let floorValue = Math.floor(4.7);
+// let a = 16
+// if (a % 2 == 0) {
+//     alert(4)
+// }
